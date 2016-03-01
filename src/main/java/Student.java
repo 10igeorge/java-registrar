62385544f6641a3e2733e86a7b65a22b033683f6
Abigail Rolling and isabelle george <arolling@gmail.com>
2016-03-01 10:41:03 -0800

    Add save and equals functions for Student including Date object

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,1 +1,59 @@
 import org.sql2o.*;
+import java.util.List;
+import java.sql.Date;
+
+
+
+public class Student {
+  private int id;
+  private String name;
+  private java.sql.Date enrollment_date;
+
+  public Student(String studentName, String enrollment){
+    name = studentName;
+    java.sql.Date enrollment_date = java.sql.Date.valueOf(enrollment);
+  }
+
+  public int getId(){
+    return id;
+  }
+
+  public String getName(){
+    return name;
+  }
+
+  public java.sql.Date getEnrollmentDate(){
+    return enrollment_date;
+  }
+
+  @Override
+  public boolean equals(Object newStudent) {
+    if (newStudent instanceof Student) {
+      Student otherStudent = (Student) newStudent;
+      return this.getName().equals(otherStudent.getName()) &&
+        this.getId() == otherStudent.getId() &&
+        this.getEnrollmentDate() == otherStudent.getEnrollmentDate();
+    } else {
+      return false;
+    }
+  }
+
+  public static List<Student> all(){
+    String sql = "SELECT * FROM students";
+    try(Connection con = DB.sql2o.open()){
+      return con.createQuery(sql)
+      .executeAndFetch(Student.class);
+    }
+  }
+
+  public void save() {
+    String sql = "INSERT INTO students (name, enrollment_date) VALUES (:name, :enrollment_date)";
+    try(Connection con = DB.sql2o.open()){
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", name)
+        .addParameter("enrollment_date", enrollment_date)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+}
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -10,6 +10,15 @@ public class StudentTest {
 
   // UNIT TESTING
 
+  @Test
+  public void all_returnsAllStudents_emptyAtFirst() {
+    assertEquals(Student.all().size(), 0);
+  }
 
-
+  @Test
+  public void save_savesStudentIntoDatabase(){
+    Student homer = new Student("Homer Simpson", "2016-02-29");
+    homer.save();
+    assertEquals(Student.all().get(0), homer);
+  }
 }
