52f317f9b7c652b0760ddb3e7753b84208d2070e
Abigail Rolling and isabelle george <arolling@gmail.com>
2016-03-01 14:50:15 -0800

    Add front end for viewing all students and courses and adding a student to a course

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -26,5 +26,56 @@ public class App {
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    post("/add-course", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      String newCourse = request.queryParams("courseName");
+      String newNumber = request.queryParams("courseNumber");
+      Course course = new Course(newCourse, newNumber);
+      course.save();
+      model.put("course", course);
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/students", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("allStudents", Student.all());
+      model.put("template", "templates/viewall.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/courses", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("allCourses", Course.all());
+      model.put("template", "templates/viewall.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/courses/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Course course = Course.find(id);
+      model.put("enrolledStudents", course.getStudents());
+      model.put("course", course);
+      model.put("allStudents", Student.all());
+      model.put("template", "templates/course.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/courses/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      int id = Integer.parseInt(request.params("id"));
+      Course course = Course.find(id);
+      int studentId = Integer.parseInt(request.queryParams("newStudent"));
+      course.addStudent(studentId);
+      Student student = Student.find(studentId);
+      model.put("addedStudent", student);
+      model.put("enrolledStudents", course.getStudents());
+      model.put("course", course);
+      model.put("allStudents", Student.all());
+      model.put("template", "templates/course.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -39,6 +39,60 @@ public class AppTest extends FluentTest {
     fill("#name").with("Mary");
     fill("#enrollmentDate").with("2000-02-22");
     submit("#createStudent");
-    assertThat(pageSource()).contains("Mary has successfully been added to the registrar");
+    assertThat(pageSource()).contains("Mary has successfully been enrolled!");
   }
+
+  @Test
+  public void addCourse(){
+    goTo("http://localhost:4567/");
+    fill("#courseName").with("Intro to Folklore");
+    fill("#courseNumber").with("FLKR1000");
+    submit("#createCourse");
+    assertThat(pageSource()).contains("FLKR1000: Intro to Folklore has successfully been added to the course list");
+  }
+
+  @Test
+  public void listAllStudents(){
+    goTo("http://localhost:4567/");
+    fill("#name").with("Mary");
+    fill("#enrollmentDate").with("2000-02-22");
+    submit("#createStudent");
+    fill("#name").with("Evan");
+    fill("#enrollmentDate").with("2000-02-21");
+    submit("#createStudent");
+    goTo("http://localhost:4567/students");
+    assertThat(pageSource()).contains("Mary");
+    assertThat(pageSource()).contains("Evan");
+  }
+
+  @Test
+  public void listAllCourses() {
+    Course economics = new Course("Basic Economics", "ECON101");
+    economics.save();
+    Course homeEconomics = new Course("Home Economics", "ECON304");
+    homeEconomics.save();
+    Course folklore = new Course("Intro to Folklore", "FLKR1000");
+    folklore.save();
+    goTo("http://localhost:4567/courses");
+    assertThat(pageSource()).contains("Basic Economics");
+    assertThat(pageSource()).doesNotContain("Mary");
+  }
+
+  @Test
+  public void addStudentSpecificCourse() {
+    Course economics = new Course("Basic Economics", "ECON101");
+    economics.save();
+    Course homeEconomics = new Course("Home Economics", "ECON304");
+    homeEconomics.save();
+    Student homer = new Student("Homer Simpson", "2016-02-29");
+    homer.save();
+    Student marge = new Student("Marge Simpson", "2016-04-29");
+    marge.save();
+    goTo("http://localhost:4567/courses");
+    click("a", withText("Home Economics"));
+    click("option", withText("Homer Simpson"));
+    submit("#enrollStudent");
+    assertThat(pageSource()).contains("Homer Simpson has been added to ECON304");
+  }
+  
 }
